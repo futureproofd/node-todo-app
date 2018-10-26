4e6311d12e7113cb59574a2ee5f5525b79b054a1
Marcus Plienegger <futureproofd@users.noreply.github.com>
2018-10-26 13:01:06 -0400

    A basic POST request using express
    express and body-parser added
    -setup a test endpoint /todos for submitting post requests
    -receive test post requests and parse body into mongoDB

--- server/server.js
+++ server/server.js
@@ -1,1 +1,32 @@
+//Server
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//DB & Model
 var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+//receive post requests from users
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+       text: req.body.text 
+    }); 
+    
+    //save mongoose-defined obj to DB
+    todo.save().then((doc) => {
+        res.send(doc); 
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+
+app.listen(3000, () => {
+   console.log('Started on Port 3000'); 
+});
